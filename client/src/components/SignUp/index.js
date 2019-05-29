458e705fc990d1bbcca00d5f17eec0141d73d731
Dialest <dialest@hotmail.com>
2019-05-28 19:53:38 -0700

    Front end validation added

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -7,7 +7,11 @@ class SignUp extends Component {
         username: "",
         email: "",
         password: "",
-        confirm: ""
+        confirm: "",
+        usernameLabel: "Username:",
+        emailLabel: "E-mail Address:",
+        passwordLabel: "Password:",
+        confirmLabel: "Confirm password:"
     }
     
     handleChange = event => this.setState({[event.target.name]: event.target.value});
@@ -16,52 +20,98 @@ class SignUp extends Component {
         event.preventDefault();
         let submission = {}
         submission.username = this.state.username;
-        if (this.props.createUser) {submission.email = this.state.email};
+        if (this.props.createUser)  { submission.email = this.state.email };
         submission.password = this.state.password;
         let url="api/users/";
         this.props.createUser ? url +="signup" : url += "login";
-        
-        axios.post(url, submission).then(response => console.log(response));
+
+        let usernameValid = true;
+        let emailValid = true;
+        let passwordVaild = true;
+        let confirmValid = true;
+
+        if (submission.username) { 
+            this.setState({ usernameLabel: "Username:"});
+        } else {
+            usernameValid = false;
+            this.setState({ usernameLabel: "Please enter a username."});
+        }
+
+        if (submission.email) {
+            let emailRegex = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            if (!emailRegex.test(String(submission.email).toLowerCase())) { 
+                emailValid = false;
+                this.setState({ emailLabel: "Please enter a valid e-mail address."});
+            } else {
+                this.setState({ emailLabel: "E-mail Address:"});
+            };
+        };
+        if (this.props.createUser) {
+            let passwordRegex = RegExp(/^(?=.{8,})(?=.*[0-9])(?=.*[A-Za-z])/);
+            if (!passwordRegex.test(String(submission.password))) { 
+                passwordVaild = false;
+                this.setState({ passwordLabel: "Password must include letters and numbers and be at least eight characters long."});
+            } else {
+                this.setState({ passwordLabel: "Password:" });
+            };
+            if (submission.password !== this.state.confirm) {
+                this.setState({ confirmLabel: "Must match password." });
+            } else {
+                this.setState({ confirmLabel: "Confirm password:" });
+            };
+        };
+
+        if (usernameValid && emailValid && passwordVaild && confirmValid) {
+            axios.post(url, submission).then(response => console.log(response));
+        }
     }
 
     render() {
         let contents = [];
         contents.push(
+            <label for="username">{this.state.usernameLabel}</label>
+        );
+        contents.push(
             <input
                 name="username"
                 key="username"
                 type="text"
-                placeholder="Enter your username*"
                 value={this.state.username} onChange={this.handleChange}
             />
         );
         if (this.props.createUser) {
             contents.push(
+                <label for="email">{this.state.emailLabel}</label>
+            );
+            contents.push(
                 <input
                     name="email"
                     key="email"
                     type="text"
-                    placeholder="Enter your e-mail address"
                     value={this.state.email} onChange={this.handleChange}
                 />
             );   
         };
         contents.push(
+            <label for="password">{this.state.passwordLabel}</label>
+        );
+        contents.push(
             <input
                 name="password"
                 key="password"
                 type="password"
-                placeholder="Enter your password*"
                 value={this.state.password} onChange={this.handleChange}
             />
         );
         if (this.props.createUser) {
             contents.push(
+                <label for="confirm">{this.state.confirmLabel}</label>
+            );
+            contents.push(
                 <input
                     name="confirm"
                     key="confirm"
                     type="password"
-                    placeholder="Re-type your password"
                     value={this.state.confirm} onChange={this.handleChange}
                 />
             );
